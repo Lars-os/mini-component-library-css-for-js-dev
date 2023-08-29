870b202ce9e7d9bffb1aea02cb56d9b9b595a8dc
Lars Olsnes Østmo-Sæter <Larsoeatmo@gmail.com>
2023-08-29 13:48:17 +0200

    progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,56 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+
+  const styles = {
+    small: {
+      "--width": 370 + "px",
+      "--height": 8 + "px",
+      "--padding": 0,
+      "--border-radius": 0
+    },
+    medium: {
+      "--width": 370 + "px",
+      "--height": 12 + "px",
+      "--padding": 0,
+      "--border-radius": 0
+    },
+    large: {
+      "--width": 370 + "px",
+      "--height": 24 + "px",
+      "--padding": 4 + "px",
+      "--border-radius": value >= 99.8 ? 4/16 + "rem" : 4/16 + "rem 0 0 " + 4/16 + "rem"
+    }
+  }
+
+
+  return <>
+    <StyledProgressBar style={styles[size]} id="loadinglabel" max="100" value={value}></StyledProgressBar>
+  </>;
 };
+
+const StyledProgressBar = styled.progress`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+
+  /* styles */
+  width: var(--width);
+  height: var(--height);
+  border-radius: ${4/16}rem;
+  border-style: none;
+  padding: var(--padding);
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: 0px 2px 4px 0px #80808059 inset;
+
+
+  &::-moz-progress-bar {
+    background-color: ${COLORS.primary};
+    border-radius: var(--border-radius);
+  }
+
+
+
+`;
 
 export default ProgressBar;
